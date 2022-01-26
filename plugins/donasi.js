14dbc9c0d479398b693a920060fbf8611eabc6c6
Bayubotzz <96980508+Bayubotzz@users.noreply.github.com>
2022-01-27 00:24:14 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,9 +1,10 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Pulsa/Gopay [089504585790]
-│ • OVO/DANA [089504685295]
-│ • Saweria https://saweria.co/mimamadi
+╭─「 Donasi • BAYUOFC 」
+│ • PULSA [081285866502]
+│ • DANA [081285866502]
+│ • GOPAY [089525518325]
 ╰────
+“Setiap orang berada di bawah naungan sedekahnya (pada hari kiamat) hingga diputuskan di antara manusia atau ia berkata: “Ditetapkan hukuman di antara manusia.” Yazid berkata: “Abul Khair tidak pernah melewati satu haripun melainkan ia bersedekah padanya dengan sesuatu, walaupun hanya sepotong kue atau bawang merah atau seperti ini.” (HR. Al-Baihqi, Al-Hakim dan Ibnu Khuzaimah).
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
